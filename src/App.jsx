46c449aa3c2b7e1416d0e00b207863954586993f
Minh Tran <115627819+minhtran5109@users.noreply.github.com>
2024-08-05 13:52:09 +1000

    move card into a separate component

--- src/App.jsx
+++ src/App.jsx
@@ -1,48 +1,28 @@
 import { useEffect, useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
+import Card from './components/Card';
 
-const BASED_API = "https://pokeapi.co/api/v2/pokemon-form"
+// const BASED_API = "https://pokeapi.co/api/v2/pokemon-form"
 
-async function fetchPokemon(pokeNumber) {
-  const response = await fetch(`${BASED_API}/${pokeNumber}`)
-  const pokeData = await response.json();
-  // console.log(pokeData);
-  return new Promise((resolve) => {
-    resolve({
-      name: pokeData.name,
-      img_src: pokeData.sprites.front_default,
-    });
-  });
-  // return pokeData;
-}
+// async function fetchPokemon(pokeNumber) {
+//   const response = await fetch(`${BASED_API}/${pokeNumber}`)
+//   const pokeData = await response.json();
+//   // console.log(pokeData);
+//   return new Promise((resolve) => {
+//     resolve({
+//       name: pokeData.name,
+//       img_src: pokeData.sprites.front_default,
+//     });
+//   });
+//   // return pokeData;
+// }
 
 function App() {
-  const [pokeData, setPokeData] = useState(null);
-  const [loading, setLoading] = useState(true);
-
-  useEffect(() => {
-    async function loadData() {
-      const result = await fetchPokemon(132);
-      setPokeData(result);
-      setLoading(false);
-    }
-    loadData();
-  }, [])
-
-  if (loading) {
-    return <div>Loading...</div>;
-  }
-
+  const example = 132;
   return (
     <>
       <h1>A game of memory</h1>
-      <div className="card">
-        <img src={pokeData.img_src}></img>
-        <span>{pokeData.name}</span>
-        <pre>{JSON.stringify(pokeData, null, 2)}</pre>
-      </div>
+      <Card pokeNumber={example}/>
     </>
   )
 }
---
+++ src/components/Card.jsx
@@ -0,0 +1,42 @@
+import { useEffect, useState } from 'react'
+
+const BASED_API = "https://pokeapi.co/api/v2/pokemon-form"
+
+async function fetchPokemon(pokeNumber) {
+  const response = await fetch(`${BASED_API}/${pokeNumber}`)
+  const pokeData = await response.json();
+  return new Promise((resolve) => {
+    resolve({
+      name: pokeData.name,
+      img_src: pokeData.sprites.front_default,
+    });
+  });
+}
+
+function Card({pokeNumber}) {
+  const [pokeData, setPokeData] = useState(null);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    async function loadData() {
+      const result = await fetchPokemon(pokeNumber);
+      setPokeData(result);
+      setLoading(false);
+    }
+    loadData();
+  }, [pokeNumber])
+
+  if (loading) {
+    return <div>Loading...</div>;
+  }
+
+  return (
+    <div className="card">
+      <img src={pokeData.img_src}></img>
+      <span>{pokeData.name}</span>
+      <pre>{JSON.stringify(pokeData, null, 2)}</pre>
+    </div>
+  )
+};
+
+export default Card;
