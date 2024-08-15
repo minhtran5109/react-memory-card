3fb2f290ef26395643c0bf502038d91fca57cfd0
Minh Tran <115627819+minhtran5109@users.noreply.github.com>
2024-08-15 10:23:19 +1000

    separate pokemon list componenet into a file

--- src/App.jsx
+++ src/App.jsx
@@ -1,43 +1,9 @@
 import { useEffect, useState } from 'react'
 import './App.css'
-import Card from './components/Card';
 import CongratulationModal from './components/CongratulationModal';
+import PokeList from './components/PokeList';
 
 const DEFAULT_POKES = [131,132,133,134,135,136];
-
-function shuffleArray(array) {
-  const newArray = array.slice(); // Create a copy of the original array
-  for (let i = newArray.length - 1; i > 0; i--) {
-      const j = Math.floor(Math.random() * (i + 1));
-      [newArray[i], newArray[j]] = [newArray[j], newArray[i]]; // Swap elements
-  }
-  return newArray;
-}
-
-const ItemList = ({ numbers , increaseCount, resetCount}) => {
-  const [shuffledNumbers, setShuffledNumbers] = useState(numbers);
-  const [clickedRecord, setClickedRecord] = useState([]);
-
-  const handleItemClick = (number) => {
-    if (!clickedRecord.includes(number)) {
-      increaseCount();
-      const newRecord = [...clickedRecord, number];
-      setClickedRecord(newRecord);
-    } else {
-      resetCount();
-      setClickedRecord([]);
-    }
-    setShuffledNumbers(shuffleArray(shuffledNumbers));
-  };
-
-  return (
-    <div className='item-list'>
-      {shuffledNumbers.map((number, index) => (
-        <Card key={index} pokeNumber={number} onClick={handleItemClick} />
-      ))}
-    </div>
-  );
-};
 
 function App() {
   const [count, setCount] = useState(0);
@@ -45,11 +11,6 @@ function App() {
   const [showModal, setShowModal] = useState(false)
 
   const targetScore = DEFAULT_POKES.length;
-  
-  // useEffect(() => {
-  //   const savedHighScore = parseInt(localStorage.getItem("highScore"), 10) || 0;
-  //   setHighScore(savedHighScore);
-  // }, [])
 
   useEffect(() => {
     if (count > highScore) {
@@ -79,7 +40,7 @@ function App() {
       <div>Score: {count} / {targetScore}</div>
       <div>Highest score: {highScore}</div>
 
-      <ItemList numbers={DEFAULT_POKES} increaseCount={handleIncreaseCount} resetCount={() => setCount(0)}/>
+      <PokeList numbers={DEFAULT_POKES} increaseCount={handleIncreaseCount} resetCount={() => setCount(0)}/>
 
       <CongratulationModal
         isVisible={showModal}
---
+++ src/components/PokeList.jsx
@@ -0,0 +1,38 @@
+import { useEffect, useState } from 'react'
+import Card from './Card';
+
+function shuffleArray(array) {
+  const newArray = array.slice(); // Create a copy of the original array
+  for (let i = newArray.length - 1; i > 0; i--) {
+      const j = Math.floor(Math.random() * (i + 1));
+      [newArray[i], newArray[j]] = [newArray[j], newArray[i]]; // Swap elements
+  }
+  return newArray;
+}
+
+const PokeList = ({ numbers , increaseCount, resetCount}) => {
+  const [shuffledNumbers, setShuffledNumbers] = useState(numbers);
+  const [clickedRecord, setClickedRecord] = useState([]);
+
+  const handleItemClick = (number) => {
+    if (!clickedRecord.includes(number)) {
+      increaseCount();
+      const newRecord = [...clickedRecord, number];
+      setClickedRecord(newRecord);
+    } else {
+      resetCount();
+      setClickedRecord([]);
+    }
+    setShuffledNumbers(shuffleArray(shuffledNumbers));
+  };
+
+  return (
+    <div className='item-list'>
+      {shuffledNumbers.map((number, index) => (
+        <Card key={index} pokeNumber={number} onClick={handleItemClick} />
+      ))}
+    </div>
+  );
+};
+
+export default PokeList;
